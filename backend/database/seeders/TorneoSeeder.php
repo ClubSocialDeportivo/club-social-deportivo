1474ce04b6601e12c02a3fd485c3a1a8869994da
Kevin Rosario <l23070017@huetamo.tecnm.mx>
2026-03-20 19:29:09 -0600

    CM3-11: Seeder de torneos listo y error de cache solucionado

--- backend/database/seeders/TorneoSeeder.php
+++ backend/database/seeders/TorneoSeeder.php
@@ -7,24 +7,20 @@ use App\Models\Torneo;
 
 class TorneoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-public function run(): void
-{
-    Torneo::create([
-        'nombre_torneo' => 'Copa Tec Morelia 2026',
-        'tipo' => 'eliminacion_directa', // <-- Palabra exacta de la migración
-        'cupo_maximo' => 16,
-        'fecha_inicio' => '2026-04-15'
-    ]);
+    public function run(): void
+    {
+        Torneo::create([
+            'nombre_torneo' => 'Copa Tec Morelia 2026',
+            'tipo' => 'eliminacion_directa',
+            'cupo_maximo' => 16,
+            'fecha_inicio' => '2026-04-15'
+        ]);
 
-    Torneo::create([
-        'nombre_torneo' => 'Liga Inter-Clubes',
-        'tipo' => 'round_robin', // <-- Palabra exacta de la migración
-        'cupo_maximo' => 10,
-        'fecha_inicio' => '2026-05-01'
-    ]);
+        Torneo::create([
+            'nombre_torneo' => 'Liga Inter-Clubes',
+            'tipo' => 'round_robin',
+            'cupo_maximo' => 10,
+            'fecha_inicio' => '2026-05-01'
+        ]);
+    }
 }
-}
-
